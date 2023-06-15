2b5f2f04b123e0be90db0818d5b8652205784d27
Tommy Smith <tommy.smith023@gmail.com>
2023-06-15 18:47:40 +0100

    add tests to cover changes to schema.exists method

--- src/schema/journey.test.ts
+++ src/schema/journey.test.ts
@@ -30,6 +30,18 @@ describe('schema', () => {
       });
   });
 
+  it('checks class existence', () => {
+    return client.schema.exists(classObj.class).then((res: boolean) => {
+      expect(res).toEqual(true);
+    });
+  });
+
+  it('checks class non-existence', () => {
+    return client.schema.exists('NonExistingClass').then((res: boolean) => {
+      expect(res).toEqual(false);
+    });
+  });
+
   it('extends the thing class with a new property', () => {
     const className = 'MyThingClass';
     const prop: Property = {
