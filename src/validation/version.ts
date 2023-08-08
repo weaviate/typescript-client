8e1d504a1913174147205531657d523e5c0f7172
Abhijithchintu <goollaa@iitbhilai.ac.in>
2023-08-08 22:43:54 +0530

    instance_version_warning

--- src/validation/version.ts
+++ src/validation/version.ts
@@ -4,7 +4,7 @@ export function isValidWeaviateVersion(version: string) {
     if (versionNumbers.length >= 2) {
       const major = parseInt(versionNumbers[0], 10);
       const minor = parseInt(versionNumbers[1], 10);
-      return major <= 1 && minor < 16 ? false : true;
+      return !(major <= 1 && minor < 16);
     }
   }
   return true;
