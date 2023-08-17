b4e6ef41a877f7cf041564caaccc4dbf249e3bb7
Andrzej Liszka <andrzej.liszka@gmail.com>
2023-08-17 12:54:40 +0200

    addresses review feedback

--- src/schema/tenantsUpdater.ts
+++ src/schema/tenantsUpdater.ts
@@ -2,7 +2,7 @@ import Connection from '../connection';
 import { CommandBase } from '../validation/commandBase';
 import { Tenant } from '../openapi/types';
 
-export default class TenantsCreator extends CommandBase {
+export default class TenantsUpdater extends CommandBase {
   private className: string;
   private tenants: Array<Tenant>;
 
--- tools/refresh_schema.sh
+++ tools/refresh_schema.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-var="${1:-master}"
-npx openapi-typescript https://raw.githubusercontent.com/weaviate/weaviate/${var}/openapi-specs/schema.json -o ./src/openapi/schema.ts
+branchOrTag="${1:-master}"
+npx openapi-typescript https://raw.githubusercontent.com/weaviate/weaviate/${branchOrTag}/openapi-specs/schema.json -o ./src/openapi/schema.ts
 npx prettier --write --no-error-on-unmatched-pattern './src/openapi/schema.ts'
