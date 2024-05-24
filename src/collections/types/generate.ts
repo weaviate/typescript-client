8538ed1d7708693fee3ff1d15c92845bdff91848
Tommy Smith <tommy@weaviate.io>
2024-05-24 10:09:55 +0100

    Add docstrings for types

--- src/collections/types/generate.ts
+++ src/collections/types/generate.ts
@@ -2,19 +2,29 @@ import { Properties } from './index.js';
 import { GroupByObject, GroupByResult, WeaviateGenericObject, WeaviateNonGenericObject } from './query.js';
 
 export type GenerativeGenericObject<T extends Properties> = WeaviateGenericObject<T> & {
+  /** The LLM-generated output applicable to this single object. */
   generated?: string;
 };
 
 export type GenerativeNonGenericObject = WeaviateNonGenericObject & {
+  /** The LLM-generated output applicable to this single object. */
   generated?: string;
 };
 
-export type GenerativeObject<T> = T extends Record<string, any>
+/** An object belonging to a collection as returned by the methods in the `collection.generate` namespace.
+ *
+ * Depending on the generic type `T`, the object will have subfields that map from `T`'s specific type definition.
+ * If not, then the object will be non-generic and have a `properties` field that maps from a generic string to a `WeaviateField`.
+ */
+export type GenerativeObject<T> = T extends Properties
   ? GenerativeGenericObject<T>
   : GenerativeNonGenericObject;
 
+/** The return of a query method in the `collection.generate` namespace. */
 export type GenerativeReturn<T> = {
+  /** The objects that were found by the query. */
   objects: GenerativeObject<T>[];
+  /** The LLM-generated output applicable to this query as a whole. */
   generated?: string;
 };
 
@@ -22,8 +32,12 @@ export type GenerativeGroupByResult<T> = GroupByResult<T> & {
   generated?: string;
 };
 
+/** The return of a query method in the `collection.generate` namespace where the `groupBy` argument was specified. */
 export type GenerativeGroupByReturn<T> = {
+  /** The objects that were found by the query. */
   objects: GroupByObject<T>[];
+  /** The groups that were created by the query. */
   groups: Record<string, GenerativeGroupByResult<T>>;
+  /** The LLM-generated output applicable to this query as a whole. */
   generated?: string;
 };
--- src/collections/types/query.ts
+++ src/collections/types/query.ts
@@ -27,18 +27,28 @@ export type QueryMetadata = 'all' | MetadataKeys | undefined;
 export type ReturnMetadata = Partial<Metadata>;
 
 export type WeaviateGenericObject<T extends Properties> = {
+  /** The generic returned properties of the object derived from the type `T`. */
   properties: ReturnProperties<T>;
+  /** The returned metadata of the object. */
   metadata: ReturnMetadata | undefined;
+  /** The returned references of the object derived from the type `T`. */
   references: ReturnReferences<T> | undefined;
+  /** The UUID of the object. */
   uuid: string;
+  /** The returned vectors of the object. */
   vectors: Vectors;
 };
 
 export type WeaviateNonGenericObject = {
+  /** The returned properties of the object. */
   properties: Record<string, WeaviateField>;
+  /** The returned metadata of the object. */
   metadata: ReturnMetadata | undefined;
+  /** The returned references of the object. */
   references: Record<string, CrossReferenceDefault> | undefined;
+  /** The UUID of the object. */
   uuid: string;
+  /** The returned vectors of the object. */
   vectors: Vectors;
 };
 
@@ -52,11 +62,16 @@ export type ReturnVectors<V> = V extends string[]
   ? { [Key in V[number]]: number[] }
   : Record<string, number[]>;
 
-export type WeaviateObject<T> = T extends Record<string, any>
-  ? WeaviateGenericObject<T>
-  : WeaviateNonGenericObject;
+/** An object belonging to a collection as returned by the methods in the `collection.query` namespace.
+ *
+ * Depending on the generic type `T`, the object will have subfields that map from `T`'s specific type definition.
+ * If not, then the object will be non-generic and have a `properties` field that maps from a generic string to a `WeaviateField`.
+ */
+export type WeaviateObject<T> = T extends Properties ? WeaviateGenericObject<T> : WeaviateNonGenericObject;
 
+/** The return of a query method in the `collection.query` namespace. */
 export type WeaviateReturn<T> = {
+  /** The objects that were found by the query. */
   objects: WeaviateObject<T>[];
 };
 
@@ -72,8 +87,11 @@ export type GroupByResult<T> = {
   objects: WeaviateObject<T>[];
 };
 
+/** The return of a query method in the `collection.query` namespace where the `groupBy` argument was specified. */
 export type GroupByReturn<T> = {
+  /** The objects that were found by the query. */
   objects: GroupByObject<T>[];
+  /** The groups that were created by the query. */
   groups: Record<string, GroupByResult<T>>;
 };
 
@@ -100,23 +118,34 @@ export type RerankOptions<T> = T extends undefined
     };
 
 interface BaseRefProperty<T> {
-  // linkOn: keyof T & string; // https://github.com/microsoft/TypeScript/issues/56239
+  /** The property to link on when defining the references traversal. */
   linkOn: RefKeys<T>;
+  /** Whether to return the vector(s) of the referenced objects in the query. */
   includeVector?: boolean | string[];
+  /** The metadata to return for the referenced objects. */
   returnMetadata?: QueryMetadata;
+  /** The properties to return for the referenced objects. */
   returnProperties?: QueryProperty<T>[];
+  /** The references to return for the referenced objects. */
   returnReferences?: QueryReference<ExtractCrossReferenceType<T[this['linkOn']]>>[];
+  /** The collection to target when traversing the references. Required for multi-target references. */
   targetCollection?: string;
 }
 
 export type RefProperty<T> = BaseRefProperty<T>;
 
 export type RefPropertyDefault = {
+  /** The property to link on when defining the references traversal. */
   linkOn: string;
+  /** Whether to return the vector(s) of the referenced objects in the query. */
   includeVector?: boolean | string[];
+  /** The metadata to return for the referenced objects. */
   returnMetadata?: QueryMetadata;
+  /** The properties to return for the referenced objects. */
   returnProperties?: (string | QueryNestedDefault)[];
+  /** The references to return for the referenced objects. */
   returnReferences?: RefPropertyDefault[];
+  /** The collection to target when traversing the references. Required for multi-target references. */
   targetCollection?: string;
 };
 
