acec9f01321dbd004da06233242d706738c38d2e
Tommy Smith <tommy@weaviate.io>
2024-10-16 12:03:38 +0100

    Fix broken tests

--- src/collections/integration.test.ts
+++ src/collections/integration.test.ts
@@ -6,6 +6,7 @@ import {
   PQConfig,
   PhoneNumber,
   PropertyConfig,
+  ReplicationDeletionStrategy,
   Text2VecContextionaryConfig,
   Text2VecOpenAIConfig,
   VectorIndexConfigHNSW,
@@ -579,7 +580,9 @@ describe('Testing of the collections.create method', () => {
     expect(response.multiTenancy.enabled).toEqual(true);
 
     expect(response.replication.asyncEnabled).toEqual(false);
-    expect(response.replication.deletionStrategy).toEqual('DeleteOnConflict');
+    expect(response.replication.deletionStrategy).toEqual<ReplicationDeletionStrategy>(
+      'NoAutomatedResolution'
+    );
     expect(response.replication.factor).toEqual(2);
 
     const indexConfig = response.vectorizers.default.indexConfig as VectorIndexConfigHNSW;
