f048d87f9bfadf29cdd8bd9f2eb7cf53d6019cfd
Pacifique Linjanja <pacilinja2@gmail.com>
2025-01-13 13:29:10 +0200

    feat: add the support of URLs for the `nearImage()` method

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { httpClient } from '../connection/http.js';
 
 const isFilePromise = (file: string | Buffer): Promise<boolean> =>
   new Promise((resolve, reject) => {
@@ -22,6 +23,29 @@ const isFilePromise = (file: string | Buffer): Promise<boolean> =>
     });
   });
 
+const isUrl = (file: string | Buffer): boolean => {
+  if (typeof file !== 'string') return false;
+  try {
+    const url = new URL(file);
+    return !!url;
+  } catch {
+    return false;
+  }
+};
+
+const downloadImageAsBase64 = async (url: string): Promise<string> => {
+  try {
+    const client = httpClient({
+      headers: { 'Content-Type': 'image/*' },
+      host: '',
+    });
+    const response = await client.externalGet(url);
+    return Buffer.from(response, 'binary').toString('base64');
+  } catch (error) {
+    throw new Error(`Failed to download image from URL: ${url}`);
+  }
+};
+
 const isBuffer = (file: string | Buffer): file is Buffer => file instanceof Buffer;
 
 const fileToBase64 = (file: string | Buffer): Promise<string> =>
@@ -37,6 +61,8 @@ const fileToBase64 = (file: string | Buffer): Promise<string> =>
         })
       : isBuffer(file)
       ? Promise.resolve(file.toString('base64'))
+      : isUrl(file)
+      ? downloadImageAsBase64(file)
       : Promise.resolve(file)
   );
 
@@ -44,7 +70,7 @@ const fileToBase64 = (file: string | Buffer): Promise<string> =>
  * This function converts a file buffer into a base64 string so that it can be
  * sent to Weaviate and stored as a media field.
  *
- * @param {string | Buffer} file The media to convert either as a base64 string, a file path string, or as a buffer. If you passed a base64 string, the function does nothing and returns the string as is.
+ * @param {string | Buffer} file The media to convert either as a base64 string, a file path string, an url, or as a buffer. If you passed a base64 string, the function does nothing and returns the string as is.
  * @returns {string} The base64 string
  */
 export const toBase64FromMedia = (media: string | Buffer): Promise<string> => fileToBase64(media);
