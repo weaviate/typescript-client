a41049c18c23950ab86849ec97f6f4849ecf9873
Tommy Smith <tommy@weaviate.io>
2025-06-10 11:01:40 +0100

    Allow `it` in `requireAtLeast`, require at least 1.26.0 in new test

--- test/version.ts
+++ test/version.ts
@@ -3,5 +3,7 @@ import { DbVersion } from '../src/utils/dbVersion';
 const version = DbVersion.fromString(`v${process.env.WEAVIATE_VERSION!}`);
 
 /** Run the suite / test only for Weaviate version above this. */
-export const requireAtLeast = (...semver: [...Parameters<DbVersion['isAtLeast']>]) =>
-  version.isAtLeast(...semver) ? describe : describe.skip;
+export const requireAtLeast =
+  (...semver: [...Parameters<DbVersion['isAtLeast']>]) =>
+  (func: jest.Describe | jest.It) =>
+    version.isAtLeast(...semver) ? func : func.skip;
