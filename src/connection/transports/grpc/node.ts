1fc0d78c43b5c7499b87b20de3617a116c41603d
Tommy Smith <tommy@weaviate.io>
2025-06-10 10:01:44 +0100

    Split grpc connection out into node and web impls

--- src/connection/transports/grpc/node.ts
+++ src/connection/transports/grpc/node.ts
@@ -1,9 +1,8 @@
 import { isAbortError } from 'abort-controller-x';
 
-import ConnectionGQL from './gql.js';
-import { InternalConnectionParams } from './transports/http.js';
+import { InternalConnectionParams } from '../http.js';
 
-import { ConsistencyLevel } from '../data/index.js';
+import { ConsistencyLevel } from '../../../data/index.js';
 
 import {
   ChannelCredentials,
@@ -14,19 +13,20 @@ import {
   Metadata,
   Status,
 } from 'nice-grpc';
+
+import {
+  HealthCheckResponse_ServingStatus,
+  HealthDefinition,
+} from '../../../proto/google/health/v1/health.js';
+import { WeaviateDefinition } from '../../../proto/v1/weaviate.js';
+
+import Aggregator, { Aggregate } from '../../../grpc/aggregator.js';
+import Batcher, { Batch } from '../../../grpc/batcher.js';
+import Searcher, { Search } from '../../../grpc/searcher.js';
+import TenantsManager, { Tenants } from '../../../grpc/tenantsManager.js';
+
 import { retryMiddleware } from 'nice-grpc-client-middleware-retry';
-
-import { HealthCheckResponse_ServingStatus, HealthDefinition } from '../proto/google/health/v1/health.js';
-import { WeaviateDefinition } from '../proto/v1/weaviate.js';
-
-import Batcher, { Batch } from '../grpc/batcher.js';
-import Searcher, { Search } from '../grpc/searcher.js';
-import TenantsManager, { Tenants } from '../grpc/tenantsManager.js';
-import { DbVersionSupport, initDbVersionProvider } from '../utils/dbVersion.js';
-
-import { WeaviateGRPCUnavailableError, WeaviateUnsupportedFeatureError } from '../errors.js';
-import Aggregator, { Aggregate } from '../grpc/aggregator.js';
-import { Meta } from '../openapi/types.js';
+import { WeaviateGRPCUnavailableError } from '../../../errors.js';
 
 export interface GrpcConnectionParams extends InternalConnectionParams {
   grpcAddress: string;
@@ -34,112 +34,6 @@ export interface GrpcConnectionParams extends InternalConnectionParams {
 }
 
 const clientFactory = createClientFactory().use(retryMiddleware);
-
-const MAX_GRPC_MESSAGE_LENGTH = 104858000; // 10mb, needs to be synchronized with GRPC server
-
-// Must extend from ConnectionGQL so that it can be passed to all the builder methods,
-// which are tightly coupled to ConnectionGQL
-export default class ConnectionGRPC extends ConnectionGQL {
-  private grpc: GrpcClient;
-
-  private constructor(params: GrpcConnectionParams & { grpcMaxMessageLength: number }) {
-    super(params);
-    this.grpc = grpcClient(params);
-  }
-
-  static use = (params: GrpcConnectionParams) => {
-    const rest = new ConnectionGQL(params);
-    const dbVersionProvider = initDbVersionProvider(rest);
-    const dbVersionSupport = new DbVersionSupport(dbVersionProvider);
-    if (params.skipInitChecks) {
-      return {
-        connection: new ConnectionGRPC({
-          ...params,
-          grpcMaxMessageLength: MAX_GRPC_MESSAGE_LENGTH,
-        }),
-        dbVersionProvider,
-        dbVersionSupport,
-      };
-    }
-    return Promise.all([
-      ConnectionGRPC.connect(
-        params,
-        (rest.get('/meta', true) as Promise<Meta>).then(
-          (res: Meta) => res.grpcMaxMessageSize || MAX_GRPC_MESSAGE_LENGTH
-        )
-      ),
-      dbVersionSupport.supportsCompatibleGrpcService().then((check) => {
-        if (!check.supports) {
-          throw new WeaviateUnsupportedFeatureError(
-            `Checking for gRPC compatibility failed with message: ${check.message}`
-          );
-        }
-      }),
-    ]).then(([connection]) => {
-      return { connection, dbVersionProvider, dbVersionSupport };
-    });
-  };
-
-  private static async connect(
-    params: GrpcConnectionParams,
-    grpcMaxLengthPromise: Promise<number>
-  ): Promise<ConnectionGRPC> {
-    const connection = await grpcMaxLengthPromise.then(
-      (grpcMaxMessageLength) =>
-        new ConnectionGRPC({
-          ...params,
-          grpcMaxMessageLength,
-        })
-    );
-    const isHealthy = await connection.grpc.health();
-    if (!isHealthy) {
-      await connection.close();
-      throw new WeaviateGRPCUnavailableError(params.grpcAddress);
-    }
-    return connection;
-  }
-
-  batch = (collection: string, consistencyLevel?: ConsistencyLevel, tenant?: string) => {
-    if (this.authEnabled) {
-      return this.login().then((token) =>
-        this.grpc.batch(collection, consistencyLevel, tenant, `Bearer ${token}`)
-      );
-    }
-    return new Promise<Batch>((resolve) => resolve(this.grpc.batch(collection, consistencyLevel, tenant)));
-  };
-
-  aggregate = (collection: string, consistencyLevel?: ConsistencyLevel, tenant?: string) => {
-    if (this.authEnabled) {
-      return this.login().then((token) =>
-        this.grpc.aggregate(collection, consistencyLevel, tenant, `Bearer ${token}`)
-      );
-    }
-    return new Promise<Aggregate>((resolve) =>
-      resolve(this.grpc.aggregate(collection, consistencyLevel, tenant))
-    );
-  };
-
-  search = (collection: string, consistencyLevel?: ConsistencyLevel, tenant?: string) => {
-    if (this.authEnabled) {
-      return this.login().then((token) =>
-        this.grpc.search(collection, consistencyLevel, tenant, `Bearer ${token}`)
-      );
-    }
-    return new Promise<Search>((resolve) => resolve(this.grpc.search(collection, consistencyLevel, tenant)));
-  };
-
-  tenants = (collection: string) => {
-    if (this.authEnabled) {
-      return this.login().then((token) => this.grpc.tenants(collection, `Bearer ${token}`));
-    }
-    return new Promise<Tenants>((resolve) => resolve(this.grpc.tenants(collection)));
-  };
-
-  close = () => {
-    this.grpc.close();
-    this.http.close();
-  };
-}
 
 export interface GrpcClient {
   aggregate: (
